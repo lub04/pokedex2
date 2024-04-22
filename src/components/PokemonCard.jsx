896940431d501e6a77c4701dba089c9f3c2efb30
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-04-22 17:11:40 +0200

    branche bulbasaur

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,8 @@
+const PokemonCard = () => {
+  return(
+  <figure>
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+    <figcaption>Bulbasaur</figcaption>
+  </figure>) 
+}
+export default PokemonCard ;
