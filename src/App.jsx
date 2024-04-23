cb877b0331246ca4eb2b047b015e3a39c8bf0dd9
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-04-23 17:20:37 +0200

    props 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import PokemonCard from "./components/PokemonCard"
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+
+  },
+
+];
 
 function App() {
   return (
       <div>
-        <PokemonCard />
+        <PokemonCard pokemon={pokemonList[0]}/>
       </div>
   )
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,24 +1,12 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
 
-  },
 
-];
-
-const PokemonCard = () => { 
-    const pokemon = pokemonList[1];
+const PokemonCard = ({ pokemon }) => { 
     return (
       <figure >
-        {pokemon.hasOwnProperty('imgSrc') ? (
+        {pokemon.imgSrc ? (
           <img src={pokemon.imgSrc} alt={pokemon.name} />
         ) : (
-          <p>ce Pokémon n'a pas d'image</p>
+          <p>???</p>
         )}
         <figcaption>{pokemon.name}</figcaption>
       </figure>
