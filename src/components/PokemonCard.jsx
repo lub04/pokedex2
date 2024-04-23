60261dbcd24c40cf222de92ca60bff3347608dc5
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-04-23 14:51:44 +0200

    nouveau composent

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,32 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+
+];
+
 const PokemonCard = () => {
-  return(
-  <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-    <figcaption>Bulbasaur</figcaption>
-  </figure>) 
-}
+  const cards = []
+  for (let i = 0; i < pokemonList.length; i++) {
+    const pokemon = pokemonList[i];
+    const card = (
+      <figure key={i}>
+        {pokemon.hasOwnProperty('imgSrc') ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>ce Pokémon n'a pas d'image</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    );
+    cards.push(card);
+  }
+
+  return <div>{cards}</div>;
+};
 export default PokemonCard ;
