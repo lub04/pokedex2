acfd98149c71abe2e621359ddc3f2434cc2de895
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-04-24 13:57:11 +0200

    debut du state 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,32 @@ import { useState } from 'react'
 import './App.css'
 import PokemonCard from "./components/PokemonCard"
 const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
 
-  },
-
-];
+  {  
+      name: "bulbasaur",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",  
+    },  
+    {
+      name: "charmander",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    }, 
+    {
+      name: "pikachu",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png", 
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 function App() {
   return (
       <div>
-        <PokemonCard pokemon={pokemonList[0]}/>
+        <PokemonCard pokemon={pokemonList[3]}/>
       </div>
   )
 }
