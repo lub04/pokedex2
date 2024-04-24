4b90a669635c245d37241211aad54a809084c5eb
Lubin Chauvreau <lubin-chauvreau@laposte.net>
2024-04-24 13:41:21 +0200

    props 2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from "prop-types";
 
 const PokemonCard = ({ pokemon }) => { 
     return (
@@ -10,6 +10,14 @@ const PokemonCard = ({ pokemon }) => {
         )}
         <figcaption>{pokemon.name}</figcaption>
       </figure>
-    );
+    ); 
 };
+
+PokemonCard.PropTypes = {
+  pokemon: PropTypes.shape({
+    imgSrc: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+  })
+}
+
 export default PokemonCard ;
